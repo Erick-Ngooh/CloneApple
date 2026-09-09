6e3bf8b2690643f789ef6482076083da95a778fe
Erick Ngooh <106706089+Erick-Ngooh@users.noreply.github.com>
2026-09-09 09:50:33 +0000

    add font css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 function App() {
-  return <h1>Hello World</h1>
+  return (
+    <>
+      <h1 className="font-[regular]">regular</h1> 
+      <h1 className="font-[medium]">medium</h1> 
+      <h1 className="font-[bold]">bold</h1> 
+      <h1 className="font-[semibold]">semibold</h1> 
+    </>
+  )
 }
 
 export default App
